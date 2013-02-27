992bcb404e3b6eddafb01f0b806b7d10cb193737
Grady Kuhnline <github@heygrady.net>
2013-02-27 14:48:04 -0800

    fixing a quotes issues. Separating the FreeMarker rules from the HTML rules.

--- freemarker_highlight_rules.js
+++ freemarker_highlight_rules.js
@@ -36,31 +36,24 @@ var lang = require("../lib/lang");
 var HtmlHighlightRules = require("./html_highlight_rules").HtmlHighlightRules;
 var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
-var FreeMarkerHighlightRules = function() {
-    // inspired by Liquid, PHP and JavaScript Highlight Rules
-
-    // repeat the HTML rules (like PHP does)
-    HtmlHighlightRules.call(this);
-
+var FreeMarkerRules = function () {
     // capture all of the freemarker keywords
     // TODO: iso...
     // @see: http://freemarker.sourceforge.net/docs/ref_builtins_date.html#ref_builtin_date_iso
     var built_ins = ("ancestors|byte|c|cap_first|capitalize|ceiling|children|chop_linebreak|chunk|contains|date|datetime|double|ends_with|eval|first|floor|groups|float|has_content|html|index_of|int|interpret|is_string|is_number|is_boolean|is_date|is_method|is_transform|is_macro|is_hash|is_hash_ex|is_sequence|is_collection|is_enumerable|is_indexable|is_directive|is_node|j_string|js_string|keys|last|last_index_of|left_pad|length|long|lower_case|matches|namespace|new|node_namespace|node_name|node_type|number|number_to_date|number_to_datetime|number_to_time|parent|replace|reverse|right_pad|round|root|rtf|short|size|sort|seq_contains|seq_index_of|seq_last_index_of|sort_by|split|starts_with|string|substring|time|trim|uncap_first|upper_case|url|values|word_list|xhtml|xml");
     
-    // TODO: User-defined directive (<@...>)
+    // TODO: special variables must be preceded by a period
     // @see: http://freemarker.sourceforge.net/docs/ref_directive_userDefined.html#ref.directive.userDefined
     var directives = ("assign|attempt|break|case|compress|default|else|elseif|escape|fallback|function|flush|ftl|global|if|import|include|list|local|lt|macro|nested|noescape|nt|recover|recurse|return|rt|setting|stop|switch|t|visit");
     var special_variables = ("data_model|error|globals|lang|locale|locals|main|namespace|node|now|output_encoding|template_name|url_escaping_charset|vars|version");
     var reserved_names = {
-    		booleans: ("true|false"),
-    		comparisons: ("gt|gte|lt|lte"),
-    		others: ("as|in|using")
-    	};
+            booleans: ("true|false"),
+            comparisons: ("gt|gte|lt|lte"),
+            others: ("as|in|using")
+        };
 
     var deprecated_directives = ("call|comment|foreach|transform");
     var deprecated_built_ins = ("default|exists|if_exists|web_safe");
-
-    // TODO: special variables must be preceded by a period
 
     var keywordMapper = this.createKeywordMapper({
         "support.function" : built_ins,
@@ -71,9 +64,6 @@ var FreeMarkerHighlightRules = function() {
         "keyword.other" : reserved_names.others,
         "invalid.deprecated" : [deprecated_directives, deprecated_built_ins].join("|")
     }, "identifier");
-
-    // regexp must not have capturing parentheses. Use (?:) instead.
-    // regexps are ordered -> the first match is used
 
     // add freemarker start tags to the HTML start tags
     this.$rules.start.unshift({
@@ -165,17 +155,18 @@ var FreeMarkerHighlightRules = function() {
             }, {
                 token : "string",
                 regex : "['\"]",
-                next : "freemarker-directive-start"
+                next : next
             }, {
                 defaultToken : "string"
             }
         ];
-        freemarker_inside.call(this, "freemarker-string-" + next);
+        //freemarker_inside.call(this, "freemarker-string-" + next);
+        
         this.$rules["freemarker-raw-string-" + next] = [
             {
                 token : "string",
                 regex : "['\"]",
-                next : "freemarker-directive-start"
+                next : next
             }, {
                 defaultToken : "string"
             }
@@ -203,11 +194,24 @@ var FreeMarkerHighlightRules = function() {
     }
 
     // Allow freemarker inside quotes
-    freemarker_inside.call(this, "tag_qqstring");
-    freemarker_inside.call(this, "tag_qstring");
+    //freemarker_inside.call(this, "tag_qqstring");
+    //freemarker_inside.call(this, "tag_qstring");
+}
+
+var FreeMarkerHighlightRules = function() {
+    // inspired by Liquid, PHP and JavaScript Highlight Rules
+
+    // repeat the HTML rules (like PHP does)
+    HtmlHighlightRules.call(this);
+
+    // regexp must not have capturing parentheses. Use (?:) instead.
+    // regexps are ordered -> the first match is used
+    FreeMarkerRules.call(this);
+
 };
 
 oop.inherits(FreeMarkerHighlightRules, TextHighlightRules);
 
+exports.FreeMarkerRules = FreeMarkerRules;
 exports.FreeMarkerHighlightRules = FreeMarkerHighlightRules;
 });
